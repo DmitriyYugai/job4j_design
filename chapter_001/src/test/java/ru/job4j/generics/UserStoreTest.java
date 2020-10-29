40280905128515366922166e8240c91ff001fa52
Dmitriy Yugai <studentnstu97@mail.ru>
2020-10-29 16:39:22 +0700

    5.2.2. Реализовать Store<T extends Base> [#394012]

---
+++ chapter_001/src/test/java/ru/job4j/generics/UserStoreTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.generics;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class UserStoreTest {
+
+    @Test
+    public void whenAdd() {
+        UserStore store = new UserStore();
+        store.add(new User("qwe"));
+        User rsl = store.findById("qwe");
+        User expected = new User("qwe");
+        assertThat(rsl, is(expected));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenFindByIdThenException() {
+        UserStore store = new UserStore();
+        store.add(new User("qwe"));
+        User rsl = store.findById("asd");
+    }
+
+    @Test
+    public void whenReplaceThenOk() {
+        UserStore store = new UserStore();
+        store.add(new User("qwe"));
+        store.add(new User("asd"));
+        boolean rsl1 = store.replace("asd", new User("zxc"));
+        User rsl2 = store.findById("zxc");
+        User expected = new User("zxc");
+        assertTrue(rsl1);
+        assertThat(rsl2, is(expected));
+    }
+
+    @Test
+    public void whenReplaceThenFail() {
+        UserStore store = new UserStore();
+        store.add(new User("qwe"));
+        store.add(new User("asd"));
+        boolean rsl1 = store.replace("asdfg", new User("zxc"));
+        User rsl2 = store.findById("asd");
+        User expected = new User("asd");
+        assertFalse(rsl1);
+        assertThat(rsl2, is(expected));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteThenOk() {
+        UserStore store = new UserStore();
+        store.add(new User("qwe"));
+        store.add(new User("asd"));
+        store.delete("asd");
+        User rsl2 = store.findById("asd");
+    }
+
+    @Test
+    public void whenDeleteThenFail() {
+        UserStore store = new UserStore();
+        store.add(new User("qwe"));
+        store.add(new User("asd"));
+        boolean rsl1 = store.delete("asdfg");
+        User rsl2 = store.findById("asd");
+        User expected = new User("asd");
+        assertFalse(rsl1);
+        assertThat(rsl2, is(expected));
+    }
+
+}
