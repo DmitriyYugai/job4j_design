2f17240ce6c21e62f91b0b26071c7ce6589a3447
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-02 16:18:08 +0700

    5. Очередь на двух стеках [#393992]

--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -8,37 +8,19 @@ public class SimpleQueue<T> {
     private SimpleStack<T> out = new SimpleStack<>();
 
     public T poll() {
-        T rsl = null;
-        while (!in.isEmpty()) {
-            out.push(in.pop());
-            if (in.isEmpty()) {
-                if (rsl != null) {
-                    SimpleStack<T> tmp2 = in;
-                    in = out;
-                    out = tmp2;
-                    break;
-                }
-                rsl = out.pop();
-                SimpleStack<T> tmp1 = in;
-                in = out;
-                out = tmp1;
+        if (in.isEmpty() && out.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
             }
         }
+        T rsl = out.pop();
         if (rsl == null) {
             throw new NoSuchElementException();
         }
         return rsl;
-//        for (T el = in.pop(); el != null; el = in.pop()) {
-//            out.push(el);
-//        }
-//        T rsl =  out.pop();
-//        for (T el = out.pop(); el != null; el = out.pop()) {
-//            in.push(el);
-//        }
-//        if (rsl == null) {
-//            throw new NoSuchElementException();
-//        }
-//        return rsl;
     }
 
     public void push(T value) {
