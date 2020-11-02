735b4f48bda858adbab90592e6f75e868a6261c8
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-02 14:04:24 +0700

    1. Создать элементарную структуру дерева [#394007]

---
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleTree.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public interface SimpleTree<E> {
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+
+    class Node<E> {
+        @SuppressWarnings("checkstyle:VisibilityModifier")
+        public final E value;
+        @SuppressWarnings("checkstyle:VisibilityModifier")
+        public final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/collection/Tree.java
@@ -0,0 +1,48 @@
+package ru.job4j.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Queue;
+
+public class Tree<E> implements SimpleTree<E> {
+    private final Node<E> root;
+
+    Tree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Optional<Node<E>> node = findBy(parent);
+        if (!node.isPresent()) {
+            return false;
+        }
+        List<Node<E>> listNodes = node.get().children;
+        for (Node<E> n : listNodes) {
+            if (n.value.equals(child)) {
+                return false;
+            }
+        }
+        listNodes.add(new Node<E>(child));
+        return true;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+
+}
