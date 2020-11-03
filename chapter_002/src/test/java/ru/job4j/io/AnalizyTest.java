504e1ffb6b4b68bf6e77be4bba0f5913c62588ec
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-03 17:14:47 +0700

    3.0. Тестирование IO [#394036]

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,25 +1,39 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.StringJoiner;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
 
     @Test
-    public void unavailable() {
+    public void whenUnavailable() throws Exception {
+        File source = tmpFolder.newFile("server.log");
+        File target = tmpFolder.newFile("unavailable.csv");
+        try (PrintWriter pw = new PrintWriter(source)) {
+            pw.write("200 10:56:01" + System.lineSeparator()
+                    + "500 10:57:01" + System.lineSeparator()
+                    + "400 10:58:01" + System.lineSeparator()
+                    + "200 10:59:01" + System.lineSeparator()
+                    + "500 11:01:02" + System.lineSeparator()
+                    + "200 11:02:02");
+        }
         Analizy analizy = new Analizy();
-        analizy.unavailable("./data/server.log", "./data/unavailable.csv");
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringJoiner out = new StringJoiner(System.lineSeparator());
-        try (BufferedReader read = new BufferedReader(new FileReader("./data/unavailable.csv"))) {
+        try (BufferedReader read = new BufferedReader(new FileReader(target))) {
             read.lines().forEach(out::add);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         assertThat(out.toString(), is(
                 "10:57:01;10:59:01" + System.lineSeparator()
