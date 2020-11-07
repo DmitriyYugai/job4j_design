ad96f2ffdad988932dcd76a572b83298934042bd
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-07 13:06:03 +0700

    5. Преобразование JSON в POJO. JsonObject [#394054]

--- chapter_002/src/main/java/ru/job4j/serialization/json/Shop.java
+++ chapter_002/src/main/java/ru/job4j/serialization/json/Shop.java
@@ -2,8 +2,12 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Shop {
     private int id;
@@ -20,6 +24,26 @@ public class Shop {
         this.codes = codes;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public boolean isOpened() {
+        return opened;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int[] getCodes() {
+        return codes;
+    }
+
     @Override
     public String toString() {
         return "Shop{"
@@ -32,22 +56,49 @@ public class Shop {
     }
 
     public static void main(String[] args) {
-        Gson gson = new GsonBuilder().create();
+//        Gson gson = new GsonBuilder().create();
+//        Shop shop = new Shop(
+//                3, true, "Shop", new Product("bread", 40), new int[]{5, 7, 10});
+//        System.out.println(gson.toJson(shop));
+//
+//        String json = "{"
+//                + "\"id\":4,"
+//                + "\"opened\":false,"
+//                + "\"name\":\"Lenta\","
+//                + "\"product\":"
+//                    + "{"
+//                            + "\"name\":\"Banana\","
+//                            + "\"price\":50"
+//                    + "},"
+//                + "\"codes\":[12, 31, 54]"
+//                + "}";
+//        System.out.println(gson.fromJson(json, Shop.class));
+
+        JSONObject jsonProduct = new JSONObject("{"
+                        + "\"name\":\"Chocolate\","
+                        + "\"price\":200"
+                        + "}"
+                );
+        System.out.println(jsonProduct);
+
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(567);
+        list.add(123);
+        list.add(678);
+        JSONArray jsonCodes = new JSONArray(list);
+        System.out.println(jsonCodes);
+
+        JSONObject jsonShop = new JSONObject();
+        jsonShop.put("id", 666);
+        jsonShop.put("opened", true);
+        jsonShop.put("name", "XXX");
+        jsonShop.put("product", jsonProduct);
+        jsonShop.put("codes", jsonCodes);
+        System.out.println(jsonShop);
+
         Shop shop = new Shop(
                 3, true, "Shop", new Product("bread", 40), new int[]{5, 7, 10});
-        System.out.println(gson.toJson(shop));
-
-        String json = "{"
-                + "\"id\":4,"
-                + "\"opened\":false,"
-                + "\"name\":\"Lenta\","
-                + "\"product\":"
-                    + "{"
-                            + "\"name\":\"Banana\","
-                            + "\"price\":50"
-                    + "},"
-                + "\"codes\":[12, 31, 54]"
-                + "}";
-        System.out.println(gson.fromJson(json, Shop.class));
+        System.out.println(new JSONObject(shop).toString());
     }
 }
