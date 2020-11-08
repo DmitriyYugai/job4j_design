280efa74d2f1c8648a0723225c6af8925e28e5fe
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-08 20:07:01 +0700

    5.2. Архивировать проект [#394046]_испр

--- chapter_002/src/main/java/ru/job4j/io/zip/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/zip/Zip.java
@@ -2,19 +2,33 @@ package ru.job4j.io.zip;
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(Path source, String exclude, Path target) throws Exception {
+    public void packFiles(List<File> list, Path target) {
        try (ZipOutputStream zip = new ZipOutputStream(
                 new BufferedOutputStream(
                         new FileOutputStream(target.toFile())))) {
-           Files.walkFileTree(source, new ZipVisitor<Path>(zip, exclude));
+           for (File file : list) {
+               zip.putNextEntry(new ZipEntry(file.getPath()));
+               try (BufferedInputStream in = new BufferedInputStream(
+                       new FileInputStream(file))) {
+                   zip.write(in.readAllBytes());
+               } catch (Exception e) {
+                   e.printStackTrace();
+               }
+           }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    public void fillList(List<File> list, Path source, String exclude) throws Exception {
+        Files.walkFileTree(source, new ZipVisitor<Path>(list, exclude));
     }
 
     public static void main(String[] args) throws Exception {
@@ -27,7 +41,10 @@ public class Zip {
         Path source = Paths.get(argZip.directory());
         String exclude = argZip.exclude();
         Path output = Paths.get(argZip.output());
-        new Zip().packFiles(source, exclude, output);
+        Zip zip = new Zip();
+        List<File> files = new ArrayList<>();
+        zip.fillList(files, source, exclude);
+        zip.packFiles(files, output);
     }
 
 }
