f5975b210ee3a5f9c914ca98ae8008d62a570efd
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-13 17:51:01 +0700

    SQL Select by company [#393979]

---
+++ chapter_003/src/main/sql_scripts/exam.sql
@@ -0,0 +1,81 @@
+CREATE TABLE company
+(
+id integer NOT NULL,
+name character varying,
+CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person
+(
+id integer NOT NULL,
+name character varying,
+company_id integer,
+CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+select * from company;
+
+select * from person;
+
+delete from person;
+delete from company;
+
+
+insert into company(id, name) values(1, 'Sberbank');
+insert into company(id, name) values(2, 'Azoft');
+insert into company(id, name) values(5, 'Epam');
+
+insert into person(id, name, company_id) values(1, 'Anton', 1);
+insert into person(id, name, company_id) values(2, 'Artem', 1);
+insert into person(id, name, company_id) values(3, 'Sveta', 2);
+insert into person(id, name, company_id) values(4, 'Vlad', 2);
+insert into person(id, name, company_id) values(5, 'Pavel', 2);
+insert into person(id, name, company_id) values(6, 'Kostya', 3);
+insert into person(id, name, company_id) values(7, 'Stas', 3);
+insert into person(id, name, company_id) values(8, 'Petr', 3);
+insert into person(id, name, company_id) values(9, 'Andrei', 3);
+
+--1) Retrieve in a single query:
+-- - names of all persons that are NOT in the company with id = 5
+select name 
+from person
+where company_id != 5;
+
+-- - company name for each person
+select p.name, c.name
+from person as p
+left join company as c 
+on p.company_id = c.id;
+
+--2) Select the name of the company with the maximum number of persons + number of persons in this company
+select *
+from (
+	select c.name, count(p.name)
+	from company as c
+	left join person as p 
+	on p.company_id = c.id
+	group by c.name
+	order by count(p.name) desc
+) as t
+limit 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
