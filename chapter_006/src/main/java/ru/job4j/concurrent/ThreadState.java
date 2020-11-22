5a21da58a49993850c7720873c076d187a39f68d
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-22 17:00:18 +0700

    1.1. Состояние нити. [#412144]_испр

--- chapter_006/src/main/java/ru/job4j/concurrent/ThreadState.java
+++ chapter_006/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,7 +1,6 @@
 package ru.job4j.concurrent;
 
 public class ThreadState {
-    @SuppressWarnings("checkstyle:EmptyBlock")
     public static void main(String[] args) {
         Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
@@ -12,8 +11,11 @@ public class ThreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-                && second.getState() != Thread.State.TERMINATED) {
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("Threads are running");
         }
+        System.out.println("State of the first thread is: " + first.getState());
+        System.out.println("State of the second thread is: " + second.getState());
         System.out.println("All threads are terminated");
     }
 }
