fa57aa471f80f1a8f3921fb58b6f2272f57ddbd5
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-24 12:54:13 +0700

    1. Реализовать шаблон Producer Consumer. [#412140]

---
+++ chapter_006/src/main/java/ru/job4j/concurrent/SimpleBlockingQueue.java
@@ -0,0 +1,46 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
+    private int maxSize = 10;
+
+    public SimpleBlockingQueue() {
+    }
+
+    public SimpleBlockingQueue(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public synchronized void offer(T value) {
+        while (queue.size() >= maxSize) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        queue.offer(value);
+        notifyAll();
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait();
+        }
+        T rsl = queue.poll();
+        notifyAll();
+        return rsl;
+    }
+
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
@@ -0,0 +1,76 @@
+package ru.job4j.concurrent;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest {
+    @Test
+    public void whenOnlyOffer() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1);
+        Thread producer = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                queue.offer(5);
+            }
+        });
+        producer.start();
+        Thread.sleep(100);
+        Thread.State rsl = producer.getState();
+        producer.interrupt();
+        assertThat(rsl, is(Thread.State.WAITING));
+    }
+
+    @Test
+    public void whenOnlyGet() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread consumer = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    queue.poll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        consumer.start();
+        Thread.sleep(100);
+        Thread.State rsl = consumer.getState();
+        consumer.interrupt();
+        assertThat(rsl, is(Thread.State.WAITING));
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(
+                            queue::offer
+                    );
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
+}
