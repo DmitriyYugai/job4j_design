bdc61e9391382d086fde5c23a4ad1fa0bc4e0b9b
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-27 21:15:19 +0700

    1. Visibility. Общий ресурс вне критической секции [#412158]_испр

--- chapter_006/src/main/java/ru/job4j/concurrent/ParseFile.java
+++ chapter_006/src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.concurrent;
 
 import java.io.*;
+import java.util.function.Predicate;
 
 public class ParseFile {
     private final File file;
@@ -10,27 +11,11 @@ public class ParseFile {
     }
 
     public synchronized String getContent() throws IOException {
-        StringBuilder output = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String data;
-            while ((data = br.readLine()) != null) {
-                output.append(data);
-            }
-        }
-        return output.toString();
+        return getContent(data -> true);
     }
 
     public synchronized String getContentWithoutUnicode() throws IOException {
-        StringBuilder output = new StringBuilder();
-        try (BufferedInputStream i = new BufferedInputStream(new FileInputStream(file))) {
-            int data;
-            while ((data = i.read()) != -1) {
-                if (data < 0x80) {
-                    output.append((char) data);
-                }
-            }
-        }
-        return output.toString();
+        return getContent(data -> data < 0x80);
     }
 
     public synchronized void saveContent(String content) throws IOException {
@@ -38,4 +23,18 @@ public class ParseFile {
             bw.write(content);
         }
     }
+
+    private String getContent(Predicate<Integer> pred) throws IOException {
+        StringBuilder output = new StringBuilder();
+        try (BufferedInputStream i = new BufferedInputStream(new FileInputStream(file))) {
+            int data;
+            while ((data = i.read()) != -1) {
+                if (pred.test(data)) {
+                    output.append((char) data);
+                }
+            }
+        }
+        return output.toString();
+    }
+
 }
